d9f3216db3d6bf96faa187cf756dfc042e127a59
Nini Antadze <nini.antadze@gmail.com>
2023-12-11 20:14:24 +0100

    made clock  updatable every second

--- src/index.js
+++ src/index.js
@@ -54,20 +54,26 @@ setInterval(function () {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
-  let cityTime = moment().tz(cityTimeZone);
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let fourCitiesElement = document.querySelector("#fourCities");
-  fourCitiesElement.innerHTML = `<div class="city">
-      <div>
-        <h2>${cityName}</h2>
-        <div class="date">${cityTime.format("MMMM Do YYYY")};
+
+  function updateTime() {
+    let cityTime = moment().tz(cityTimeZone);
+    fourCitiesElement.innerHTML = `<div class="city">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date">${cityTime.format("MMMM Do YYYY")};
+          </div>
+        </div>
+        <div class="time">
+          ${cityTime.format("h:mm:ss [<small>]A[</small>]")}
         </div>
       </div>
-      <div class="time">
-        ${cityTime.format("h:mm:ss [<small>]A[</small>]")}
-      </div>
-    </div>
-  `;
+    `;
+  }
+
+  updateTime();
+  setInterval(updateTime, 1000);
 }
 
 let citySelectElement = document.querySelector("#oneCity");
